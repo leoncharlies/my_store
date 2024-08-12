477ff8e10ce86d312f1720811551d1979bca7546
Somnus <chen1921460502@outlook.com>
2024-08-12 18:05:34 +0800

    完善了查找红色激光笔点的逻辑，但是并没有完成校准，校准可能需要固定场地才行

--- 23/main.py
+++ 23/main.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 from utils import initlize_camera,read_frame
-from follow_rect import find_blobs,draw_on_image
+from find_blob import find_blobs_random,draw_on_image_rect
 
 class catcher:
     def __init__(self):
@@ -10,13 +10,14 @@ class catcher:
     def main(self):
         while True:
             frame=read_frame(self.cap)
-            blob=find_blobs(frame)
-            img=draw_on_image(frame,blob)
+            blob=find_blobs_random(frame)
+            img=draw_on_image_rect(frame,blob)
             cv2.imshow('frame',img)
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
         self.cap.release()
         cv2.destroyAllWindows()
+    
 
 
 c=catcher()

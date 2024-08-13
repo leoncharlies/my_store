139a80d07442c42344ae42339f4e768d30b68f06
Somnus <chen1921460502@outlook.com>
2024-08-13 17:13:28 +0800

    更新了一下名字

---
+++ 23/catcher.py
@@ -0,0 +1,31 @@
+from find_blob import find_blobs, find_blobs_random, draw_on_image_rect, find_laser_point_position
+from utils import initlize_camera
+import cv2
+
+class catcher:
+    def __init__(self):
+        self.cap = initlize_camera(0)
+        self.corner_location=[]
+        self.is_write_down=False
+
+    def find_blob(self,stop_event):
+        while not stop_event.is_set():
+            ret,frame=self.cap.read()
+            if not ret:
+                print("Can't receive frame (stream end?). Exiting ...")
+                break
+            result = find_laser_point_position(frame)
+            if self.is_write_down:
+                self.corner_location.append(result)
+                self.is_write_down=False
+            cv2.imshow('frame', frame)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+            stop_event.wait(0.01)
+        self.exit() 
+    
+    def exit(self):
+        self.cap.release()
+        cv2.destroyAllWindows()
+
+t = catcher()
